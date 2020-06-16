556750aa361251d7c60194711d209d15a9d7271b
Resha Puspita <reshapuspita734@gmail.com>
2020-06-16 15:25:22 +0700

    add files

---
+++ index.js
@@ -0,0 +1,6 @@
+const h1 = document.getElementById("h1");
+console.log(h1);
+h1.style.color = "white";
+h1.style.background = "blue";
+h1.style.fontSize = "40px";
+h1.style.fontFamily = "monospace";
